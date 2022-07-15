7b7241be7290bc00f1f0d1f1e514afa81d2b0c0c
FirasALhinde <firasalhinde98@gmail.com>
2022-07-16 01:28:30 +0300

    add blog modle

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from.models import Post
-from django.utils import timezone
+from.models import Post,Category
+
 # Register your models here.
 
-
-admin.site.register(Post)
+class PostAmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category')
+    search_fields = ['title','content']
+    date_hierarchy = 'publish_date'
+admin.site.register(Post,PostAmin)
+admin.site.register(Category)
--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,26 @@
-
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField(max_length=10000) 
+    content = models.TextField(max_length=10000,null=True,blank=True) 
+    publish_date = models.DateField(default=timezone.now)
+    update_date = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to = 'posts/')
+    category = models.ForeignKey(to='Category',on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+        ordering = ['publish_date']
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    class Meta:
+        verbose_name = "category"
+        verbose_name_plural = "categories"
+    def __str__(self):
+            return self.name
